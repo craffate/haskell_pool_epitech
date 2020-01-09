a96a33a22445101ab77371bdc119854a96f26f6f
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2020-01-09 03:29:38 +0100

    Add step 02, task 06, add comments

--- FUN_day1_2020/My.hs
+++ FUN_day1_2020/My.hs
@@ -1,11 +1,20 @@
+-- Step 01
+-- Task 01
 mySucc x = x + 1
+-- Task 02
 myIsNeg x = x < 0
+-- Task 03
 myAbs x = case x < 0 of
         True -> x * (-1)
         False -> x
+-- Task 04
 myMin x y = case x < y of
         True -> x
         False -> y
+-- Task 05
 myMax x y = case x < y of
         True -> y
         False -> x
+-- Step 02
+-- Task 06
+myTuple x y = (x, y)
